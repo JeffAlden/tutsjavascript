657eacd672ea6c8659a6385670c4ef4a6d83326b
Jeffrey <jpalino@highlysucceed.com>
2024-05-25 16:46:52 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// falsy values -> 0, "", undefined, null, NaN
+// const num1 = Boolean(185);
+// console.log(num1);
+// console.log(typeof num1);
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean("Hi I am a string"));
+
+// const num1 = 15 .toString();
+// const num2 = String(15);
+// const num3 = (15).toString();
+// const num4 = Number("15");
+// console.log(num1);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof num3);
+// console.log(typeof num4);
+// const num1 = 15;
+// const string1 = `My string number is ${num1}`;
+// console.log(string1);
+
+
+
+
+// const result = "15" + 5 - 25 / 5;
+// console.log(result);
+
+
+// const num1 = "14";
+// const num2 = "7";
+// // console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+
+// const num1 = Math.round("135.998");
+// console.log(num1);
+// console.log(typeof num1);
+
+// const num2 = 15;
+// console.log(num2);
+// console.log(typeof num2);
+
